ee78eeccce9afdacb0b3a8d0b1a3af5ceb7d38dd
bd_ <bd_@nadena.dev>
2024-10-06 14:19:43 -0700

    wip

---
+++ src/check-changelog.ts
@@ -0,0 +1,23 @@
+import { Octokit } from '@octokit/action'
+import * as core from '@actions/core'
+import { restEndpointMethods } from '@octokit/plugin-rest-endpoint-methods'
+
+const MyOctokit = Octokit.plugin(restEndpointMethods)
+const octokit = new MyOctokit()
+
+export async function check_changelog(): Promise<void> {
+  const combined_repo = core.getInput('repository')
+  const pull_number = core.getInput('pull_request_id')
+
+  const [owner, repo] = combined_repo.split('/')
+
+  const pull = await octokit.rest.pulls.get({
+    owner,
+    repo,
+    pull_number: +pull_number
+  })
+
+  //const body = pull.data
+
+  core.debug('body:\n' + JSON.stringify(pull.data, null, 2))
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 /**
  * The entrypoint for the action.
  */
-import { run } from './main'
+import * as core from '@actions/core'
+
+import { check_changelog } from './check-changelog.js'
+
+export async function run(): Promise<void> {
+  try {
+    const command: string = core.getInput('command')
+
+    switch (command) {
+      case 'check-changelog':
+        await check_changelog()
+        break
+      default:
+        core.setFailed('Unknown command: ' + command)
+        break
+    }
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 run()
